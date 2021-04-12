7c11322bd74063a7c7407a400bbb517a43d342f9
Raafm <72469894+Raafm@users.noreply.github.com>
2021-04-12 16:57:21 -0300

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,62 @@
+mini_graph = [
+ [
+     [6,0.96 ,White ],
+     [6, 0.08 ,White ],         # 'en'   node 0 
+     [7, 0.64 ,White ],
+     [8,0.41 ,White ],
+     [10,0.96,White]   
+ ]  ,                                     
+ [
+     [6,0.008 ,White ],
+     [7, 0.91 ,White ],          # 'de'     node 1 
+     [10, 0.005,White]                   
+ ]  ,                          
+ [
+     [6, 0.003 ,White ],
+     [7,0.07 ,White ],          # 'it'    node 2 
+     [10,0.003,White]                    
+ ]  ,                    
+ [                              # 'ru'     node 3  
+     [7,0.06 ,White ],
+     [9,0.94,White]                                 
+ ]  ,                                           
+ [
+     [7,0.18  ,White ],         # 'fr'     node 4 
+     [10,0.006,White]                              
+ ]  ,                         
+ [
+     [11,0.021 ,White ],        # 'zh'     node 5
+     [12,0.9,White]                               
+ ]  ,                        
+ [ 
+     [1,0.008 ,White ],         # 'BR'     node 6 
+     [0, 0.08 ,White ],
+     [2, 0.003,White]                    
+ ]  ,                            
+ [
+     [1, 0.91 ,White ],         # 'DE'     node 7
+     [0,0.64 ,White ],
+     [4, 0.18 ,White ],
+     [2, 0.07 ,White ],
+      [3, 0.06,White] 
+ ]  ,     
+ [
+     [0, 0.19,White]            # IN     node 8                                  
+ ]  ,                                    
+ [
+     [3, 0.94,White]            # 'RU'     node 9                  
+ ]  ,                                                            
+ [
+     [1, 0.005 ,White ],        # 'US'    node 10
+     [0, 0.96 ,White ],
+     [4,0.006 ,White ],
+      [2, 0.003,White]        
+ ]  ,                    
+ [
+     [0, 0.96  ,White ],        # 'AU'     node 11
+     [5,0.021,White]                              
+ ]  ,                        
+ [
+     [5, 0.9,White]             # 'CN'     node 12            
+ ]                             
+]
